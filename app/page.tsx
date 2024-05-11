57385cc4ea88ca8c79101f80b6fc1082200cfbd9
heyjun3 <t3k2g5kako@yahoo.co.jp>
2024-05-11 13:01:43 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,3 +3,4 @@ import React from 'react'
 export default function Page() {
   return <h1>Top Page</h1>;
 }
+
---
+++ app/react-test/dom.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export function Form() {
+  const [value, setValue] = useState("Change me");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(event.currentTarget.value);
+  }
+
+  return (
+    <>
+      <input value={value} onChange={handleChange} />
+      <p>Value: {value}</p>
+    </>
+  );
+}
